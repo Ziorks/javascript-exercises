24187d4ab355aacd954072a88318e1d9e698dd77
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2023-12-05 13:23:53 -0600

    Completed 1-7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,x) {
+    let result = '';
+    if(x < 0){
+        return 'ERROR'
+    }
+    for(let i = 0; i < x; i++ ){
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
